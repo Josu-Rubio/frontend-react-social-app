8a738988dca62e07e7168a83dc8f1ec4730cc370
Josué Rubio <josu_mr@hotmail.com>
2022-01-11 13:27:29 +0100

    finished frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
-  return <Profile />;
+  return <Home />;
 }
 
 export default App;
